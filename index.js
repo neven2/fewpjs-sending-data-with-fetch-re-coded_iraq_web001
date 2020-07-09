23834e439f3782deb51e85ec8b8671aa3cc3db04
Neven Bedr <neven2225@gmail.com>
2020-07-09 19:09:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name,email){
+  return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: "Steve",
+    email: "steve@steve.com"
+  })
+})
+.then(res => res.json())
+.then(res => {
+
+
+    let newNode = document.createElement('p');
+    newNode.innerHTML = res.id;
+    document.querySelector("body").appendChild(newNode);
+
+
+}).catch(error=>{
+  let newNode = document.createElement('p');
+  newNode.innerHTML = error.message;
+  document.querySelector("body").appendChild(newNode);
+
+})
+}
